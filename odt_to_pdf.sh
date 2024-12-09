8c5c6e40290864c2612d223d1c6bb732f863a8b1
Mathijs Pittoors <79464596+JustPassingBy06@users.noreply.github.com>
2024-12-09 15:56:59 +0100

    HC 9/12/2024

--- odt_to_pdf.sh
+++ odt_to_pdf.sh
@@ -2,4 +2,6 @@
 
 echo "Converting all .odt in the current folder files to .pdf's. Please wait, this might take a while."
 time libreoffice --convert-to pdf *.odt
-echo "Done :)"
+git add .
+git status
+echo "Done, you may commit now :)"
